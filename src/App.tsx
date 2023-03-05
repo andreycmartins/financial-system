889a968180f92c34ed0742835004fcc6c37c302c
Andrey Martins <Andreybr45@gmail.com>
2023-03-05 00:49:03 -0300

    desenvolvido parte da tabela

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import * as C from "./App.styles"
 import { Item } from "./types/Item"
-import { Category } from "./types/Category"
-import { categories } from "./data/categories"
 import { items } from "./data/items"
 import { useEffect, useState } from "react"
-import { getCurrentMonth } from "./helpers/dateFilter"
+import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter"
+import { TableArea } from "./components/TableArea"
 
 const App = () => {
   const [list, setList] = useState(items)
@@ -12,7 +11,7 @@ const App = () => {
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
 
   useEffect(() => {
-    
+    setFilteredList(filterListByMonth(list, currentMonth))
   },[list, currentMonth])
 
   return (
@@ -26,6 +25,7 @@ const App = () => {
         {/* Área de inserção */}
 
         {/* Tabela de itens */}
+        <TableArea list={filteredList}/>
       </C.Body>
     </C.Container>
   )
